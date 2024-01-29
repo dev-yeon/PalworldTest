35fb49a2cc1c8f356479c2e47c39f97d8b9bb309
dev-yeon <110480577+dev-yeon@users.noreply.github.com>
2024-01-29 11:23:29 +0900

    Initial Commit

---
+++ PalworldYeon/PalworldYeonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalworldYeonApp.swift
+//  PalworldYeon
+//
+//  Created by yeon I on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct PalworldYeonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
